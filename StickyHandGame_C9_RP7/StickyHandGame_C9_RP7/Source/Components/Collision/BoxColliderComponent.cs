fa2ec3655a6298cd0d9fb9c54d01e7b7b9370f8f
KentPhan <kent.chi.lam@gmail.com>
2018-08-22 04:24:24 -0600

    messing with collisions before going to sleep

--- StickyHandGame_C9_RP7/StickyHandGame_C9_RP7/Source/Components/Collision/BoxColliderComponent.cs
+++ StickyHandGame_C9_RP7/StickyHandGame_C9_RP7/Source/Components/Collision/BoxColliderComponent.cs
@@ -10,18 +10,34 @@ namespace StickyHandGame_C9_RP7.Source.Components.Collision
 {
     public class BoxColliderComponent : CollisionComponent
     {
-        private Point _center;
+        private Vector2 _center;
 
         private float _width;
 
         private float _height;
 
 
-        public BoxColliderComponent(Point center, float width, float height)
+        public BoxColliderComponent(Vector2 center, float width, float height)
         {
             _center = center;
             _width = width;
             _height = height;
+        }
+
+        public bool DoesCollide(BoxColliderComponent otherBox)
+        {
+            float halfWidth = _width / 2;
+            float halfHeight = _height / 2;
+            Vector2 BLeft = new Vector2(_center.X - halfWidth, _center.Y + halfHeight);
+            Vector2 BRight = new Vector2(_center.X + halfWidth, _center.Y + halfHeight);
+            Vector2 TLeft = new Vector2(_center.X - halfWidth, _center.Y - halfHeight);
+            Vector2 TRight = new Vector2(_center.X + halfWidth, _center.Y - halfHeight);
+
+
+
+
+
+            return false;
         }
     }
 }
