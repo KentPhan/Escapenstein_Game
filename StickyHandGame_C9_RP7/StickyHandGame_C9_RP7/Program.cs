35c4f8af5acc125c632fe16e0c57cafc38744302
KENT CHI PHAN <u0734504@ad.utah.edu>
2018-08-21 18:36:49 -0600

    First commit

---
+++ StickyHandGame_C9_RP7/StickyHandGame_C9_RP7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StickyHandGame_C9_RP7
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
